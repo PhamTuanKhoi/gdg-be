117ca86e825c55384096681a474a01491d20db46
Pham Tuan Khoi <huynhanhpham734@gmail.com>
2025-02-28 09:38:30 +0700

    delete module takeaway-infor

--- src/takeaway-infor/dto/update-takeaway-infor.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTakeawayInforDto } from './create-takeaway-infor.dto';
-
-export class UpdateTakeawayInforDto extends PartialType(CreateTakeawayInforDto) {}
--- src/takeaway-infor/takeaway-infor.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TakeawayInforService } from './takeaway-infor.service';
-import { TakeawayInforController } from './takeaway-infor.controller';
-
-@Module({
-  controllers: [TakeawayInforController],
-  providers: [TakeawayInforService],
-})
-export class TakeawayInforModule {}
