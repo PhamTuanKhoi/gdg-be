ef32282e2b8a1b78f05e6f8139fd516b491c6063
Pham Tuan Khoi <huynhanhpham734@gmail.com>
2025-03-08 14:08:55 +0700

    setup connect socket calibration

--- src/websockets/websockets.gateway.ts
+++ src/websockets/websockets.gateway.ts
@@ -1,34 +1,22 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
+} from '@nestjs/websockets';
 import { WebsocketsService } from './websockets.service';
-import { CreateWebsocketDto } from './dto/create-websocket.dto';
-import { UpdateWebsocketDto } from './dto/update-websocket.dto';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ namespace: 'calibration', path: '/s' })
 export class WebsocketsGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly websocketsService: WebsocketsService) {}
 
-  @SubscribeMessage('createWebsocket')
-  create(@MessageBody() createWebsocketDto: CreateWebsocketDto) {
-    return this.websocketsService.create(createWebsocketDto);
-  }
-
-  @SubscribeMessage('findAllWebsockets')
-  findAll() {
-    return this.websocketsService.findAll();
-  }
-
-  @SubscribeMessage('findOneWebsocket')
-  findOne(@MessageBody() id: number) {
-    return this.websocketsService.findOne(id);
-  }
-
-  @SubscribeMessage('updateWebsocket')
-  update(@MessageBody() updateWebsocketDto: UpdateWebsocketDto) {
-    return this.websocketsService.update(updateWebsocketDto.id, updateWebsocketDto);
-  }
-
-  @SubscribeMessage('removeWebsocket')
-  remove(@MessageBody() id: number) {
-    return this.websocketsService.remove(id);
+  @SubscribeMessage('calibration')
+  handleData(@ConnectedSocket() client: Socket): void {
+    this.server.emit('sync', { data: 'data' });
   }
 }
